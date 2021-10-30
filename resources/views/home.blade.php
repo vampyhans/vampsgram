775d31e20e76ffed20afabac379aa1acb0204914
Vampy Hans <hansahimasarawijesinghe@gmail.com>
2021-10-30 08:04:48 +0530

    working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/logo.svg" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div><h1>Jaffery Woods</h1></div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>666</strong>posts</div>
+                <div class="pr-3"><strong>1m</strong>followers</div>
+                <div class="pr-3"><strong>1</strong>following</div>
             </div>
+            <div class="pt-4">your nightmare begins here :)</div>
+            <div><a href="http://jeffrywoods.com">http://jeffrywoods.com</a></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="" alt="">
         </div>
     </div>
 </div>
